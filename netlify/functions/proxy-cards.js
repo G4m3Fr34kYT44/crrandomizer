23df6668567bb4fcd11c8e7848ea3b33372ace1e
G4m3Fr34kYT44 <g4m3fr34kyt@gmail.com>
2025-06-22 15:31:55 +0200

    test

--- netlify/functions/proxy-cards.js
+++ netlify/functions/proxy-cards.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context) {
   const response = await fetch("https://api.clashroyale.com/v1/cards", {
     headers: {
-      Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJjZjkyOGU2LTkzZmEtNDRiMS1iMTFhLTIxMjlmZDViZDNjNCIsImlhdCI6MTc1MDU5NzEwMCwic3ViIjoiZGV2ZWxvcGVyLzYxNzJhZWZhLTczMTctM2E0Ny1hMTg5LTA1MGM4OGI1OWQ4MyIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.uC8PdEMJRb412_3WedXwD_AyEg_q4gr_L3FssonrZ4ZEmbG3HfL-2YvReOWrWsSxqXZy8auoKfNh6rAyLXu_Xg`,
+      Authorization: `Bearer ${process.env.API_KEY}`,
     },
   });
 
